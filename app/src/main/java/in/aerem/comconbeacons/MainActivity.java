44869f171f02b568be519fa7966f36bfb7a87965
Alexey Eremin <a.eremin.msu@gmail.com>
2019-02-04 21:14:09 +0100

    Add dynamic list stub

--- app/src/main/java/in/aerem/comconbeacons/MainActivity.java
+++ app/src/main/java/in/aerem/comconbeacons/MainActivity.java
@@ -3,16 +3,23 @@ package in.aerem.comconbeacons;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.AlertDialog;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import org.altbeacon.beacon.*;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_COARSE_LOCATION = 1;
@@ -21,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             // Android M Permission check
             if (this.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -28,5 +36,16 @@ public class MainActivity extends AppCompatActivity {
                         PERMISSION_REQUEST_COARSE_LOCATION);
             }
         }
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        MutableLiveData<List<String>> liveData = new MutableLiveData<>();
+        UsersPositionsAdapter adapter = new UsersPositionsAdapter();
+        liveData.observe(this, data -> adapter.setData(data));
+        liveData.postValue(Arrays.asList("Foo", "Bar", "Quz"));
+
+        recyclerView.setAdapter(adapter);
     }
 }
---
+++ app/src/main/java/in/aerem/comconbeacons/UsersPositionsAdapter.java
@@ -0,0 +1,48 @@
+package in.aerem.comconbeacons;
+
+import android.arch.lifecycle.LiveData;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsersPositionsAdapter extends RecyclerView.Adapter<UsersPositionsAdapter.ViewHolder> {
+    private List<String> mDataset = new ArrayList<>();
+
+    public void setData(List<String> newData) {
+        this.mDataset = newData;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(android.R.layout.simple_list_item_1, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.mTextView.setText(mDataset.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+}
