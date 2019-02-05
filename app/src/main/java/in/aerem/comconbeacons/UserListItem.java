f100882e87eba2e87d89d2779bfdb15038ab2968
Alexey Eremin <a.eremin.msu@gmail.com>
2019-02-06 00:29:10 +0100

    Make time output less ugly

--- app/src/main/java/in/aerem/comconbeacons/UserListItem.java
+++ app/src/main/java/in/aerem/comconbeacons/UserListItem.java
@@ -1,6 +1,16 @@
 package in.aerem.comconbeacons;
 
+import org.ocpsoft.prettytime.PrettyTime;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class UserListItem {
+    static SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
     public String username;
     public String location;
     public String time;
@@ -8,6 +18,17 @@ public class UserListItem {
     public UserListItem(UsersResponse r) {
         username = r.email;
         location = r.beacon == null ? "None" : r.beacon.bssid;
-        time = r.updated_at;
+        time = humanReadableDateInfo(r.updated_at);
+    }
+
+    private static String humanReadableDateInfo(String rawDate) {
+        format.setTimeZone(TimeZone.getTimeZone("UTC"));
+        try {
+            PrettyTime p = new PrettyTime(new Locale("ru"));
+            Date date = format.parse(rawDate);
+            return p.format(date);
+        } catch (ParseException e) {
+            return "";
+        }
     }
 }
