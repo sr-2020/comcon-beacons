dc8184c178d1cd422c681cbca5c239650cf6b4c3
Alexey Eremin <a.eremin.msu@gmail.com>
2019-03-20 02:15:58 +0100

    Add Bugfender integration

--- app/src/main/java/in/aerem/comconbeacons/ComConBeaconsApplication.kt
+++ app/src/main/java/in/aerem/comconbeacons/ComConBeaconsApplication.kt
@@ -3,9 +3,18 @@ package `in`.aerem.comconbeacons
 import android.app.Application
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import com.bugfender.sdk.Bugfender
 
 class ComConBeaconsApplication : Application() {
     fun getGlobalSharedPreferences(): SharedPreferences {
         return PreferenceManager.getDefaultSharedPreferences(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Bugfender.init(this, "LqR51nrg2rygHhlHefCwf0HIgG4F04DB", BuildConfig.DEBUG)
+        Bugfender.enableCrashReporting()
+        Bugfender.enableUIEventLogging(this)
+        Bugfender.enableLogcatLogging()
     }
 }
